627974e3edb107a5f024727881556c900d32992a
garryaklia31 <158801619+garryaklia31@users.noreply.github.com>
2024-02-19 09:40:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 ///https://api.telegram.org/bot<token>/setwebhook?url=<url>
 
-$botToken = "1472905848:AAHj-wDq3UJKkCL61VnuK7w-zOPxadnKjlI"; // Enter ur bot token
+$botToken = "6986904145:AAH3uB4Uee6-z48Plmc1iK1Mn-axcqC0CkA"; // Enter ur bot token
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
